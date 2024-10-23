fa26d9f023c58e369600854fea48511bb7b1ff27
Javier París <javierparis39@hotmail.com>
2024-10-23 11:40:55 +0200

    Primer commit

---
+++ src/main/java/travel/w2m/pruebaw2m/PruebaW2mApplication.java
@@ -0,0 +1,13 @@
+package travel.w2m.pruebaw2m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaW2mApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaW2mApplication.class, args);
+	}
+
+}
---
+++ src/test/java/travel/w2m/pruebaw2m/PruebaW2mApplicationTests.java
@@ -0,0 +1,13 @@
+package travel.w2m.pruebaw2m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaW2mApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
